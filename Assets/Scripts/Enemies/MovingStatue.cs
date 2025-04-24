649ca812f674bc329b069811b02227e61c73ce3c
BeluuDaVinci <maria.mounier@davinci.edu.ar>
2025-04-24 18:42:57 -0300

    Added new enemy for testing

---
+++ Assets/Scripts/Enemies/MovingStatue.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class WeepingAngel : MonoBehaviour
+{
+    public NavMeshAgent ai;
+    
+    public Transform player;
+    
+    Vector3 _dest;
+    
+    public Camera playerCam, jumpscareCam;
+    
+    public float aiSpeed;
+    
+    public float catchDistance;
+    
+    public float jumpscareTime;
+    
+    public string sceneAfterDeath;
+    
+    void Update()
+    {
+ 
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(playerCam);
+
+  
+        float distance = Vector3.Distance(transform.position, player.position);
+
+   
+        if(GeometryUtility.TestPlanesAABB(planes, this.gameObject.GetComponent<Renderer>().bounds))
+        {
+            ai.speed = 0;
+            ai.SetDestination(transform.position);
+        }
+        
+        if (!GeometryUtility.TestPlanesAABB(planes, this.gameObject.GetComponent<Renderer>().bounds))
+        {
+            ai.speed = aiSpeed;
+            _dest = player.position;
+            ai.destination = _dest;
+            
+            if(distance <= catchDistance)
+            {
+                player.gameObject.SetActive(false);
+                jumpscareCam.gameObject.SetActive(true);
+                StartCoroutine(KillPlayer());
+            }
+        }
+    }
+
+    IEnumerator KillPlayer()
+    {
+        yield return new WaitForSeconds(jumpscareTime);
+        SceneManager.LoadScene(sceneAfterDeath);
+    }
+}
