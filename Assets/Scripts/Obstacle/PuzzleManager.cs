4c04109bb5859f31b99b6f81c278d53438cd3dd5
TheMordex <melidonienzo@gmail.com>
2025-05-08 19:20:17 -0300

    Puzzle Manager Rework

--- Assets/Scripts/Obstacle/PuzzleManager.cs
+++ Assets/Scripts/Obstacle/PuzzleManager.cs
@@ -1,30 +1,58 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public class PuzzleData
+{
+    public string name;
+    public GameObject door;
+    public int requiredBoxes = 2;
+    [HideInInspector] public int placedBoxes = 0;
+    [HideInInspector] public bool completed = false;
+    public AudioClip doorOpenSound;
+}
 
 public class PuzzleManager : MonoBehaviour
 {
     public static PuzzleManager Instance;
-    public GameObject puerta;
-    private int cajasColocadas = 0;
 
-    void Awake()
+    [Header("Puzzle List")]
+    public List<PuzzleData> puzzles = new List<PuzzleData>();
+
+    private void Awake()
     {
+        // Singleton pattern
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
         Instance = this;
     }
 
-    public void BoxPlaced()
+    public void PlaceBoxInPuzzle(int puzzleIndex)
     {
-        cajasColocadas++;
-        if (cajasColocadas >= 2)
-        {
-            AbrirPuerta();
-        }
-    }
+        // Valid index check
+        if (puzzleIndex < 0 || puzzleIndex >= puzzles.Count) return;
 
-    void AbrirPuerta()
-    {
-        puerta.SetActive(false);
-        Debug.Log("abrio xd");
+        PuzzleData puzzle = puzzles[puzzleIndex];
+
+        // Avoid repeating completion
+        if (puzzle.completed) return;
+
+        puzzle.placedBoxes++;
+
+        if (puzzle.placedBoxes >= puzzle.requiredBoxes)
+        {
+            puzzle.completed = true;
+
+            // Deactivate door
+            if (puzzle.door != null)
+                puzzle.door.SetActive(false);
+
+            // Play sound
+            if (puzzle.doorOpenSound != null && puzzle.door != null)
+                AudioSource.PlayClipAtPoint(puzzle.doorOpenSound, puzzle.door.transform.position);
+        }
     }
 }
--- Assets/Scripts/Obstacle/PuzzleZone.cs
+++ Assets/Scripts/Obstacle/PuzzleZone.cs
@@ -1,25 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PuzzleZone : MonoBehaviour
 {
-    public Transform snapPoint; 
-    public bool isOccupied = false;
+    public Transform snapPoint;
+    public int puzzleIndex; 
+    public AudioClip placeBoxSound;
+
+    private bool isOccupied = false;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!isOccupied && other.CompareTag("PuzzleBox"))
+        if (!isOccupied && other.GetComponent<Box>() != null)
         {
             Rigidbody rb = other.GetComponent<Rigidbody>();
             if (rb != null) rb.isKinematic = true;
-            
-            // posiciona el objeto al snappoint que seria el punto que vos colocas
+
             other.transform.position = snapPoint.position;
             other.transform.rotation = snapPoint.rotation;
 
             isOccupied = true;
-            PuzzleManager.Instance.BoxPlaced(); 
+
+            if (placeBoxSound != null)
+                audioSource.PlayOneShot(placeBoxSound);
+
+            PuzzleManager.Instance.PlaceBoxInPuzzle(puzzleIndex);
         }
     }
 }
