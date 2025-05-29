0a28c808a55193cff344c70a955f40c8aef9da18
Leo Gonzalez <66876432+NeoLez@users.noreply.github.com>
2025-05-29 00:33:17 -0300

    Unlocking spells

--- Assets/Scripts/Drawing/DrawingPatternDatabase.cs
+++ Assets/Scripts/Drawing/DrawingPatternDatabase.cs
@@ -1,16 +1,22 @@
+using System.Collections.Generic;
 using Optional;
 using UnityEngine;
 
 public static class DrawingPatternDatabase {
     private static readonly DrawingPatternSO[] Patterns = Resources.LoadAll<DrawingPatternSO>("Patterns");
+    private static readonly HashSet<DrawingPatternSO> UnlockedPatterns = new();
 
     public static Option<CardInfoSO> GetSpellFromDrawing(Drawing drawing) {
-        foreach (var pattern in Patterns) {
+        foreach (var pattern in UnlockedPatterns) {
             if (pattern.drawing.Equals(drawing)) {
                 return pattern.cardInfo.SomeNotNull();
             }
         }
 
         return Option.None<CardInfoSO>();
+    }
+
+    public static void UnlockSpell(DrawingPatternSO drawingPattern) {
+        UnlockedPatterns.Add(drawingPattern);
     }
 }
---
+++ Assets/Scripts/Drawing/UnlockSpellOnSolvePuzzle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(DrawingSurfacePuzzle))]
+public class UnlockSpellOnSolvePuzzle : MonoBehaviour {
+    [SerializeField] private List<DrawingPatternSO> DrawingPatterns; 
+    private void OnEnable() {
+        GetComponent<DrawingSurfacePuzzle>().OnPuzzleSolved += UnlockSpell;
+    }
+
+    private void OnDisable() {
+        GetComponent<DrawingSurfacePuzzle>().OnPuzzleSolved -= UnlockSpell;
+    }
+
+    private void UnlockSpell() {
+        foreach (var pattern in DrawingPatterns) {
+            DrawingPatternDatabase.UnlockSpell(pattern);
+        }
+    }
+}
