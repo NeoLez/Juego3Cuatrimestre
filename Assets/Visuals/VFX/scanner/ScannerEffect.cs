f24ccd54fcf491cff232b5f4394e80ad302a174a
Leo Gonzalez <66876432+NeoLez@users.noreply.github.com>
2025-06-01 17:32:17 -0300

    Optimized scanner logic to avoid reevaluating objects

--- Assets/Visuals/VFX/scanner/ScannerEffect.cs
+++ Assets/Visuals/VFX/scanner/ScannerEffect.cs
@@ -5,41 +5,49 @@ public class ScannerEffect : MonoBehaviour
 {
     public float maxScanRadius = 10f;
     public float scanDuration = 2f;
-    public string targetTag = "Scanneable";
+    private List<ScannableObject> hits = new();
+    private bool isScanning;
+    private float timer = 0;
+    private float currentRadius = 0;
+    private int lastIndexAnalyzed = -1;
 
-    private float currentRadius = 0f;
-    private float timer = 0f;
-    private bool isScanning = true;
-    private HashSet<Collider> alreadyDetected = new();
+    private void Start() {
+        isScanning = true;
+        
+        foreach (var collider in Physics.OverlapSphere(transform.position, maxScanRadius)) {
+            if (collider.TryGetComponent(out ScannableObject scannable)) {
+                hits.Add(scannable);
+            }
+        }
+        hits.Sort((a, b) => {
+            return (int)((transform.position - a.transform.position).sqrMagnitude -
+                    (transform.position - b.transform.position).sqrMagnitude);
+        });
+    }
 
     void Update()
     {
-        if (!isScanning) return;
-
-        timer += Time.deltaTime;
-        currentRadius = Mathf.Lerp(0f, maxScanRadius, timer / scanDuration);
-
-        Collider[] hits = Physics.OverlapSphere(transform.position, currentRadius);
-        foreach (Collider hit in hits)
-        {
-            if (!alreadyDetected.Contains(hit) && hit.CompareTag(targetTag))
-            {
-                var scannable = hit.GetComponent<ScannableObject>();
-                if (scannable != null)
-                {
-                    scannable.OnScanned();
+        if (isScanning) {
+            timer += Time.deltaTime;
+            currentRadius = Mathf.Lerp(0f, maxScanRadius, timer / scanDuration);
+            
+            while (true) {
+                if (lastIndexAnalyzed >= hits.Count - 1) {
+                    Destroy(gameObject);
+                    break;
                 }
-                alreadyDetected.Add(hit);
+
+                if ((transform.position - hits[lastIndexAnalyzed + 1].transform.position).magnitude <= currentRadius) {
+                    lastIndexAnalyzed++;
+                    hits[lastIndexAnalyzed].OnScanned();
+                }
+                else {
+                    break;
+                }
             }
         }
-
-        if (timer >= scanDuration)
-        {
-            isScanning = false;
-            Destroy(gameObject); // opcional: destruye el scanner cuando termina
-        }
     }
-
+    
     void OnDrawGizmos()
     {
         if (isScanning)
