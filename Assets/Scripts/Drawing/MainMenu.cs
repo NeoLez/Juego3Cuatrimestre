d26a1f49a367ee405ed9229ecc9424a7acbb6c29
Leo Gonzalez <66876432+NeoLez@users.noreply.github.com>
2025-06-10 18:51:43 -0300

    Load faster from menu (unresponsive menu :c)

--- Assets/Scripts/Drawing/MainMenu.cs
+++ Assets/Scripts/Drawing/MainMenu.cs
@@ -1,10 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
 
 public class MainMenu : MonoBehaviour {
     [SerializeField] private GameObject controles;
+    [SerializeField] private string sceneKey;
+    private AsyncOperationHandle<SceneInstance> loadHandle;
+    private bool sceneLoadedToMemory;
+    private bool shouldLoadScene;
+    private Scene mainMenuScene;
+
+    private void Start() {
+        mainMenuScene = SceneManager.GetActiveScene();
+        
+        loadHandle = Addressables.LoadSceneAsync(
+            sceneKey, 
+            LoadSceneMode.Single, 
+            activateOnLoad: false
+        );
+
+        loadHandle.Completed += handle => {
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+                sceneLoadedToMemory = true;
+        };
+    }
+
+    private void Update() {
+        if (shouldLoadScene && sceneLoadedToMemory) { 
+            loadHandle.Result.ActivateAsync();
+            
+            shouldLoadScene = false;
+        }
+    }
 
     public void ShowControles() {
         controles.SetActive(true);
@@ -12,9 +42,8 @@ public class MainMenu : MonoBehaviour {
     public void HideControles() {
         controles.SetActive(false);
     }
-    public void Play()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    public void Play() {
+        shouldLoadScene = true;
     }
     
     public void Quit()
