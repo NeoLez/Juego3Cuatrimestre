01df43c9844ee7360d099af5778d1fa8ef9d799d
Leo Gonzalez <66876432+NeoLez@users.noreply.github.com>
2025-06-11 01:56:34 -0300

    Primer Intento Puzzle rotatorio

---
+++ Assets/Scripts/RotateProjectionPlane.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RotateProjectionPlane : MonoBehaviour {
+    private bool isPlayerInRange;
+    private bool firstRotation;
+
+    [SerializeField] private DrawingPuzzleArea planeToRotate;
+    void Update()
+    {
+        if (isPlayerInRange && Keyboard.current.eKey.wasPressedThisFrame && !planeToRotate.IsRotating())
+        {
+            Debug.Log("aa");
+            
+            if (firstRotation) {
+                planeToRotate.rotation = new Vector3(-90,0,0);
+            }
+            else {
+                planeToRotate.rotation = new Vector3(90,0,0);
+            }
+
+            firstRotation = !firstRotation;
+        }
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            isPlayerInRange = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            isPlayerInRange = false;
+    }
+}
