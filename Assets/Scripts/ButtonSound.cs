539c9f449588b9c03d7280a317921477896051ce
TheMordex <melidonienzo@gmail.com>
2025-06-24 22:30:02 -0300

    juas juas

--- Assets/Scripts/ButtonSound.cs
+++ Assets/Scripts/ButtonSound.cs
@@ -1,29 +1,93 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
+using System.Collections;
 
-[RequireComponent(typeof(AudioSource))]
-public class ButtonSound : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+public class ButtonSound : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
+    private static AudioSource globalAudioSource;
+
     public AudioClip hoverSound;
     public AudioClip clickSound;
+    
+    public GameObject panelOpcionConfirmar;  
+    public GameObject panelSalirConfirmar;    
 
-    private AudioSource audioSource;
+    public bool abreOpciones = false;
+    public bool abreSalir = false;
+    public bool cierraOpciones = false;
+    public bool cierraSalir = false;
 
-    void Awake()
+    private TextMeshProUGUI buttonText;
+    public Color hoverColor = Color.yellow;
+    private Color originalColor;
+
+    private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.playOnAwake = false;
+        if (globalAudioSource == null)
+        {
+            GameObject audioManager = GameObject.Find("AudioManager");
+            if (audioManager == null)
+            {
+                audioManager = new GameObject("AudioManager");
+                globalAudioSource = audioManager.AddComponent<AudioSource>();
+                DontDestroyOnLoad(audioManager);
+            }
+            else
+            {
+                globalAudioSource = audioManager.GetComponent<AudioSource>();
+            }
+        }
+
+        buttonText = GetComponentInChildren<TextMeshProUGUI>();
+        if (buttonText != null)
+            originalColor = buttonText.color;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (hoverSound != null)
-            audioSource.PlayOneShot(hoverSound);
+            globalAudioSource.PlayOneShot(hoverSound);
+
+        if (buttonText != null)
+            buttonText.color = hoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (buttonText != null)
+            buttonText.color = originalColor;
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         if (clickSound != null)
-            audioSource.PlayOneShot(clickSound);
+            globalAudioSource.PlayOneShot(clickSound);
+
+        if (buttonText != null)
+            buttonText.color = originalColor;
+
+        if (abreOpciones && panelOpcionConfirmar != null)
+        {
+            StartCoroutine(DelaySetActive(panelOpcionConfirmar, true));
+        }
+        else if (abreSalir && panelSalirConfirmar != null)
+        {
+            StartCoroutine(DelaySetActive(panelSalirConfirmar, true));
+        }
+        else if (cierraOpciones && panelOpcionConfirmar != null)
+        {
+            StartCoroutine(DelaySetActive(panelOpcionConfirmar, false));
+        }
+        else if (cierraSalir && panelSalirConfirmar != null)
+        {
+            StartCoroutine(DelaySetActive(panelSalirConfirmar, false));
+        }
+    }
+
+    private IEnumerator DelaySetActive(GameObject panel, bool state)
+    {
+        yield return new WaitForSeconds(clickSound.length);
+        panel.SetActive(state);
     }
 }
